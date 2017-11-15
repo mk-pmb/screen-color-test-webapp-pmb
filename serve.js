b81bc33f8eedec13ccd895d87850efcd98af8a0b
mk-pmb <mk-pmb@users.noreply.github.com>
2017-11-15 04:42:46 +0100

    http-listen

--- serve.js
+++ serve.js
@@ -1,9 +1,4 @@
 ﻿/*jslint indent: 2, maxlen: 80, node: true */
-/* -*- tab-width: 2 -*- */
 'use strict';
-
 module.exports = require('ecstatic')({ root: require('absdir')(module) });
-if (require.main === module) {
-  require('http').createServer(module.exports
-    ).listen(+process.env.PORT || 8080);
-}
+require('http-listen')(module).loudListen();
